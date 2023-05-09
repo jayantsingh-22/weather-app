dcb5c95a4060381b2914adffeb1de78dc7466d44
jayantsingh-22 <jayantsingh640@gmail.com>
2023-05-09 10:48:08 +0530

    form method modified

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/", async (req, res) => {
+app.post("/weather", async (req, res) => {
   const { city } = req.body;
 
   if (!city) {
